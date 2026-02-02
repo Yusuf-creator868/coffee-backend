c1826c66d3319c7b1472a6647e862d072936060f
Yusuf <yakubovyusuf821@gmail.com>
2026-02-02 11:39:46 +0500

    Setting fix

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-dev-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,coffee-backend-wik5.onrender.com").split(",")
 
 
 # Application definition
